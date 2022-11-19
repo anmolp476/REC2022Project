fd7308046422cc386e091bc758cc614d9c53b84b
Abdullah2650 <118619988+Abdullah2650@users.noreply.github.com>
2022-11-19 12:34:39 -0500

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -12,25 +12,23 @@
     type: 'line',
     data: {
       labels: [
-        '2020',
-        '2030',
-        '2040',
-        '2050',
-        '2060',
-        '2070',
-        '2080',
-        '2090',
-        '2100'
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
       ],
       datasets: [{
         data: [
-          7000000,
-          8000000,
-          9000000,
-          10000000,
-          11000000,
-          12000000,
-          
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
         ],
         lineTension: 0,
         backgroundColor: 'transparent',
@@ -53,4 +51,3 @@
     }
   })
 })()
-
